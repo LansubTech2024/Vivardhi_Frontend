1f227322c9cf2849e921a9462298ef9a4bbdb332
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-10-09 15:57:04 -0700

    changes added

--- src/Pages/Resource/Resource.jsx
+++ src/Pages/Resource/Resource.jsx
@@ -22,18 +22,22 @@ function GetData() {
         <>
           <Header/>
           <Sidebar/>
-          <div className='getdata-container'>
+          <div className='resource-container'>
             <h2>Resources Data</h2>
             <table className='table-container'>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Name</th>
+                        <th></th>
+                        <th>Working</th>
+                        <th>Worked</th>
+                        <th>Leave</th>
                         <th>Working Hours</th>
                         <th colSpan="2">Shift</th>
                         <th>Allocated/Not Allocated</th>
                     </tr>
                     <tr>
+                        <th></th>
+                        <th></th>
                         <th></th>
                         <th></th>
                         <th></th>
@@ -45,13 +49,15 @@ function GetData() {
                 <tbody>
                     {data.map((item) => (
                         <tr key={item.id}>
-                            <td>{item.id}</td>
                             <td>{item.name}</td>
+                            <td>{item.working}</td>
+                            <td>{item.worked}</td>
+                            <td>{item.leave}</td>
                             <td>{item.working_hours}</td>
                             <td>{item.shift === 'Day' ?'✔️' : ''}</td>
                             <td>{item.shift === 'Night' ?'✔️' : ''}</td>
-                            <td style={{color : item.allocated ? 'green' : 'red'}}>
-                            {item.allocated ? "Yes" : "No"}
+                            <td style={{color : item.allocated === 'Yes' ? 'green' : 'red'}}>
+                            {item.allocated}
                             </td>
                         </tr>
                     ))}
